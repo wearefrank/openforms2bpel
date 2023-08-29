b82cbf1cac2f45af73d3eeea018b8abccdea8ea1
MLenterman <marcellenterman@hotmail.com>
2023-08-29 16:10:26 +0200

    build properties

---
+++ .github/workflows/writeBuildInfo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ -n $1 ]]; then
+  echo "writeBuildInfo.sh - writing version $1"
+  echo "instance.version=$1" > src/main/resources/BuildInfo.properties
+  echo "versionDate_ddmmyyyy=$(date +%d/%m/%Y)" >> src/main/resources/BuildInfo.properties
+else
+  echo "writeBuildInfo.sh - no version to write, leaving BuildInfo.properties unchanged"
+fi

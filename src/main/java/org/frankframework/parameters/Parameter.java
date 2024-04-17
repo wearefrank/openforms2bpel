34a66a48303e7de5634187a7d75169392e14107f
MLenterman <marcellenterman@hotmail.com>
2024-04-17 14:25:51 +0200

    build(dependencies): bump f!f version to 8.1.0-RC2-20240411.160355

--- src/main/java/org/frankframework/parameters/Parameter.java
+++ src/main/java/org/frankframework/parameters/Parameter.java
@@ -120,7 +120,6 @@ public class Parameter implements IConfigurable, IWithParameters {
 	public static final String TYPE_DATE_PATTERN="yyyy-MM-dd";
 	public static final String TYPE_TIME_PATTERN="HH:mm:ss";
 	public static final String TYPE_DATETIME_PATTERN="yyyy-MM-dd HH:mm:ss";
-
 	public static final String TYPE_TIMESTAMP_PATTERN= DateFormatUtils.FORMAT_FULL_GENERIC;
 
 	public static final String FIXEDUID ="0a1b234c--56de7fa8_9012345678b_-9cd0";
@@ -581,7 +580,7 @@ public class Parameter implements IConfigurable, IWithParameters {
 						authType = appConstants.getProperty("openforms2bpel.objects-api.auth-type", ""); // "jwt", "basic", "value"
 						authAlias = appConstants.getProperty("openforms2bpel.objects-api.auth-alias", "");
 					} else {
-						throw new ParameterException("Parameter ["+getName()+"] unable to resolve ["+strResult+"] to a known api type");
+						throw new ParameterException(getName(), "unable to resolve ["+strResult+"] to a known api type");
 					}
 
 					CredentialFactory credentialFactory = new CredentialFactory(authAlias);
@@ -601,7 +600,7 @@ public class Parameter implements IConfigurable, IWithParameters {
 					} else if ("value".equalsIgnoreCase(authType)){
 						result = secret;
 					} else {
-						throw new ParameterException("Parameter ["+getName()+"] unknown auth-type ["+authType+"], must be 'jwt', 'basic' or 'value'");
+						throw new ParameterException(getName(), "unknown auth-type ["+authType+"], must be 'jwt', 'basic' or 'value'");
 					}
 				}
 			} else {
@@ -622,10 +621,15 @@ public class Parameter implements IConfigurable, IWithParameters {
 		}
 
 		if (result instanceof Message) { //we just need to check if the message is null or not!
-			if (Message.isNull((Message)result)) {
+			Message resultMessage = (Message) result;
+			if (Message.isNull(resultMessage)) {
 				result = null;
-			} else if (((Message)result).asObject() instanceof String) { //Used by getMinLength and getMaxLength
-				result = ((Message) result).asObject();
+			} else if (resultMessage.isRequestOfType(String.class)) { //Used by getMinLength and getMaxLength
+				try {
+					result = resultMessage.asString();
+				} catch (IOException ignored) {
+					// Already checked for String, so this should never happen
+				}
 			}
 		}
 		if (result != null && !"".equals(result)) {
@@ -709,18 +713,17 @@ public class Parameter implements IConfigurable, IWithParameters {
 		Message requestMessage = Message.asMessage(request);
 		Object result = request;
 		try {
-			Object requestObject = requestMessage.asObject();
 			switch(getType()) {
 				case NODE:
 					try {
 						if (isRemoveNamespaces()) {
 							requestMessage = XmlUtils.removeNamespaces(requestMessage);
 						}
-						if(requestObject instanceof Document) {
-							return ((Document)requestObject).getDocumentElement();
+						if(request instanceof Document) {
+							return ((Document)request).getDocumentElement();
 						}
-						if(requestObject instanceof Node) {
-							return requestObject;
+						if(request instanceof Node) {
+							return request;
 						}
 						result = XmlUtils.buildDomDocument(requestMessage.asInputSource(), namespaceAware).getDocumentElement();
 						final Object finalResult = result;
@@ -734,8 +737,8 @@ public class Parameter implements IConfigurable, IWithParameters {
 						if (isRemoveNamespaces()) {
 							requestMessage = XmlUtils.removeNamespaces(requestMessage);
 						}
-						if(requestObject instanceof Document) {
-							return requestObject;
+						if(request instanceof Document) {
+							return request;
 						}
 						result = XmlUtils.buildDomDocument(requestMessage.asInputSource(), namespaceAware);
 						final Object finalResult = result;
@@ -748,8 +751,8 @@ public class Parameter implements IConfigurable, IWithParameters {
 				case DATETIME:
 				case TIMESTAMP:
 				case TIME: {
-					if (requestObject instanceof Date) {
-						return requestObject;
+					if (request instanceof Date) {
+						return request;
 					}
 					Message finalRequestMessage = requestMessage;
 					LOG.debug("Parameter [{}] converting result [{}] to Date using formatString [{}]", this::getName, () -> finalRequestMessage, this::getFormatString);
@@ -762,8 +765,8 @@ public class Parameter implements IConfigurable, IWithParameters {
 					break;
 				}
 				case XMLDATETIME: {
-					if (requestObject instanceof Date) {
-						return requestObject;
+					if (request instanceof Date) {
+						return request;
 					}
 					Message finalRequestMessage = requestMessage;
 					LOG.debug("Parameter [{}] converting result [{}] from XML dateTime to Date", this::getName, () -> finalRequestMessage);
@@ -771,8 +774,8 @@ public class Parameter implements IConfigurable, IWithParameters {
 					break;
 				}
 				case NUMBER: {
-					if (requestObject instanceof Number) {
-						return requestObject;
+					if (request instanceof Number) {
+						return request;
 					}
 					Message finalRequestMessage = requestMessage;
 					LOG.debug("Parameter [{}] converting result [{}] to number decimalSeparator [{}] groupingSeparator [{}]", this::getName, ()->finalRequestMessage, decimalFormatSymbols::getDecimalSeparator, decimalFormatSymbols::getGroupingSeparator);
@@ -786,8 +789,8 @@ public class Parameter implements IConfigurable, IWithParameters {
 					break;
 				}
 				case INTEGER: {
-					if (requestObject instanceof Integer) {
-						return requestObject;
+					if (request instanceof Integer) {
+						return request;
 					}
 					Message finalRequestMessage = requestMessage;
 					LOG.debug("Parameter [{}] converting result [{}] to integer", this::getName, ()->finalRequestMessage);
@@ -799,8 +802,8 @@ public class Parameter implements IConfigurable, IWithParameters {
 					break;
 				}
 				case BOOLEAN: {
-					if (requestObject instanceof Boolean) {
-						return requestObject;
+					if (request instanceof Boolean) {
+						return request;
 					}
 					Message finalRequestMessage = requestMessage;
 					LOG.debug("Parameter [{}] converting result [{}] to boolean", this::getName, ()->finalRequestMessage);
@@ -858,7 +861,7 @@ public class Parameter implements IConfigurable, IWithParameters {
 	}
 
 	private Object preFormatDateType(Object rawValue, String formatType, String patternFormatString) throws ParameterException {
-		if (formatType!=null && (formatType.equalsIgnoreCase("date") || formatType.equalsIgnoreCase("time"))) {
+		if (formatType!=null && ("date".equalsIgnoreCase(formatType) || "time".equalsIgnoreCase(formatType))) {
 			if (rawValue instanceof Date) {
 				return rawValue;
 			}
@@ -891,16 +894,22 @@ public class Parameter implements IConfigurable, IWithParameters {
 		Object substitutionValue = paramValue == null ? null : paramValue.getValue();
 
 		if (substitutionValue == null) {
-			Message substitutionValueMessage = session.getMessage(name);
-			if (!substitutionValueMessage.isEmpty()) {
-				if (substitutionValueMessage.asObject() instanceof Date) {
-					substitutionValue = preFormatDateType(substitutionValueMessage.asObject(), formatType, formatString);
+			Object substitutionValueMessage = session.get(name);
+			if (substitutionValueMessage != null) {
+				if (substitutionValueMessage instanceof Date) {
+					substitutionValue = preFormatDateType(substitutionValueMessage, formatType, formatString);
 				} else {
-					try {
-						substitutionValue = substitutionValueMessage.asString();
-					} catch (IOException e) {
-						throw new ParameterException(getName(), "Cannot get substitution value", e);
+					if (substitutionValueMessage instanceof String) {
+						substitutionValue = substitutionValueMessage;
+					} else {
+						try {
+							Message message = Message.asMessage(substitutionValueMessage); // Do not close object from session here; might be reused later
+							substitutionValue = message.asString();
+						} catch (IOException e) {
+							throw new ParameterException(getName(), "Cannot get substitution value from session key: " + name, e);
+						}
 					}
+					if (substitutionValue == null) throw new ParameterException(getName(), "Cannot get substitution value from session key: " + name);
 				}
 			}
 		}
